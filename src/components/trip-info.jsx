7e6d8d5e648d3934e247eebf4172344e7a535319
Will <wmplqnjr@gmail.com>
2023-05-21 11:42:02 -0700

    new pages

---
+++ src/components/trip-info.jsx
@@ -0,0 +1,23 @@
+
+function TripInfo(){
+
+    return (
+        <div className="trip-info">
+
+            <div>
+                <h5>Pick up</h5>
+                {/* Add a Date select tool */}
+                {/* add a location picker */}
+            </div>
+
+            <div>
+            <h5>Drop off</h5>
+                {/* Add a Date select tool */}
+                {/* add a location picker */}
+            </div>
+
+        </div>
+    );
+}
+
+export default TripInfo;

61605206d1784a3e0e0eb530ea3ee595039cc68d
Brayan Ornelas <ornelas.brayan@uabc.edu.mx>
2023-05-25 18:50:21 -0700

    Links Header

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,11 +1,33 @@
 import { Link } from "react-router-dom";
+import './footer.css';
 
 function Footer(){
+
+    function privacy(){
+        console.log("Privacy button");
+    }
+
+    function terms(){
+        console.log("Terms of service button");
+    }
+
+    function aboutUs(){
+        console.log("About us button");
+    }
+
+    function help(){
+        console.log("Help button");
+    }
 
     return (
         // links and pages need to be made for "privacy", "Terms of Service", "About us", "help"
         <div className="footer">
-            
+            <div className="buttons">
+                <button className="btn" onClick={privacy}>Privacy</button>
+                <button className="btn" onClick={terms}>Terms of service</button>
+                <button className="btn" onClick={aboutUs}>About us</button>
+                <button className="btn" onClick={help}>Help</button>
+            </div>
         </div>
     );
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,26 +1,15 @@
-import "./header.css";
 import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css'
+import "./header.css";
 
 function Heading(){
 
-    function homepage(){
-        console.log("Home clicked");
-    }
-
-    function manageBooking(){
-        console.log("Manage clicked");
-    }
-
     return (
         <div className="header">
-            <div className="buttons">
-                <button className="btn" onClick={homepage}>Rentalcars.com</button>
-                <button className="btn btn-success" onClick={manageBooking}>Manage booking</button>
-            </div>
-            {/* <div>
+            <nav>
+                <Link className="btn" to="/">Rentalcars.com</Link>
                 <Link className="btn btn-success" to="/login">Manage booking</Link>
-            </div> */}
+            </nav>
         </div>
     );
 }

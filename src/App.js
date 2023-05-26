fa3273ddd7a6ca15e4466ca74a2cbd823a5bad4b
Will <wmplqnjr@gmail.com>
2023-05-25 19:49:00 -0700

    catalog route

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Privacy from './pages/privacy';
 import Terms from './pages/terms';
 import About from './pages/about';
 import Help from './pages/help';
+import Catalog from './pages/catalog';
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
@@ -20,6 +21,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/catalog" element= {<Catalog />} />
             <Route path="/privacy" element={<Privacy />} />
             <Route path="/terms" element={<Terms />} />
             <Route path="/about" element={<About />} />

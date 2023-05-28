3e88bd88cafafa9d43f4191baa6b1d76d4d8e219
Brayan Ornelas <ornelas.brayan@uabc.edu.mx>
2023-05-28 11:08:56 -0700

    Privacy page updated

--- src/pages/privacy.jsx
+++ src/pages/privacy.jsx
@@ -27,7 +27,36 @@ function Privacy(){
             <p><b>Marketing and Promotions:</b> To send you promotional materials, newsletters, and special offers related to our rental car services. You can unsubscribe from these communications at any time.</p>
             <p><b>Legal Compliance:</b> To comply with applicable laws, regulations, and legal processes.</p>
 
+            <h4>4. Data Usage and Sharing</h4>
+            <p>We respect your privacy and will not sell, rent, or share your personal information with third parties for their marketing purposes without your explicit consent. However, we may share your information with trusted third-party service providers and business partners who assist us in operating our website, processing payments, managing reservations, or conducting business activities on our behalf. These entities are obligated to maintain the confidentiality and security of your information.</p>
+            <p>We may also disclose your information if required by law, government authorities, or to protect our legal rights, safety, or property.</p>
 
+            <h4>5. Data security</h4>
+            <p>We take the security of your personal information seriously. We implement appropriate technical, administrative, and physical safeguards to protect against unauthorized access, disclosure, alteration, or destruction of your data. However, no method of transmission over the internet or electronic storage is completely secure. Therefore, while we strive to protect your information, we cannot guarantee its absolute security.</p>
+
+            <h4>6. Data retention</h4>
+            <p>We retain your personal information for as long as necessary to fulfill the purposes outlined in this Privacy Policy, unless a longer retention period is required or permitted by law. When your data is no longer needed, we will securely dispose of it in accordance with applicable laws and regulations.</p>
+
+            <h4>7. User rights</h4>
+            <p>You have the right to access, update, correct, or delete your personal information. If you would like to exercise any of these rights, or if you have any questions or concerns about our privacy practices, please contact us using the information provided below.</p>
+
+            <h4>8. Cookies and Tracking Technologies</h4>
+            <p>Our website uses cookies and similar tracking technologies to enhance your browsing experience. You can manage your cookie preferences through your browser settings or opt-out of targeted advertising by visiting the Digital Advertising Alliance's opt-out page (http://www.aboutads.info/choices) or the Network Advertising Initiative's opt-out page (http://www.networkadvertising.org/choices).</p>
+
+            <h4>9. Links to Third-Party Websites</h4>
+            <p>Our website may contain links to third-party websites or services. This Privacy Policy applies solely to our website, and we have no control over the privacy practices or content of these linked sites. We encourage you to review the privacy policies of any third-party sites you visit.</p>
+
+            <h4>10. Updates to the Privacy Policy</h4>
+            <p>We may update this Privacy Policy periodically to reflect changes in our information practices. We will notify you of any material changes by posting the updated policy on our website with a revised "Effective Date" at the top of the page. We encourage you to review this Privacy Policy regularly for any updates.</p>
+
+            <h4>11. Contact Information</h4>
+            <p>If you have any questions, comments, or concerns about this Privacy Policy or our privacy practices, please contact us at:</p>
+            <p><b>Rentalcar</b></p>
+            <p><b>San Diego, California</b></p>
+            <p><b>rentalcar@gmail.com</b></p>
+            <p><b>(619)621-2121</b></p>
+
+            <h4>Thank you for entrusting us with your personal information. We are committed to protecting your privacy and providing you with a safe and enjoyable experience on our rental car website.</h4>
 
         </div>
     );

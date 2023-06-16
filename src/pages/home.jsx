a0d76ee1542765f9bbeaa5cc52cc962822325fb2
Brayan Ornelas <ornelas.brayan@uabc.edu.mx>
2023-06-15 19:54:20 -0700

    CSS Home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,8 @@
 import "./home.css";
 import { Link } from 'react-router-dom'
 import DataService from "../services/dataService";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck, faMagnifyingGlass, faCalendarDays, faClock } from '@fortawesome/free-solid-svg-icons'
 
 function Home() {
 
@@ -14,23 +16,59 @@ function Home() {
 
         <div className="site-info">
             <h1>Car Rental - Search, Compare & Book</h1>
-            <h4>Free cancelation</h4>
-            <h4>Customer support</h4>
+            <h4><FontAwesomeIcon icon={faCheck} /> Free cancelation</h4>
+            <h4><FontAwesomeIcon icon={faCheck} /> Customer support</h4>
         </div>
 
 
         <div className="container">
-            <input class="sep form-control form-control-lg" type="text" placeholder="Pick-up location"/>
-            <input class="sep form-control form-control-lg" type="text" placeholder="Pick-up date"/>
-            <input class="sep form-control form-control-lg" type="text" placeholder="Time"/>
-            <input class="sep form-control form-control-lg" type="text" placeholder="Drop-off date"/>
-            <input class="sep form-control form-control-lg" type="text" placeholder="Time"/>
-            
+            <div className="inputs">
+
+                <div className="wrapper pb-1">
+                    <div className="icon">
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </div>
+                    <input class="sep form-control form-control-lg input ps-5" type="text" placeholder="Pick-up location"/>
+                </div>
+
+                <div className="wrapper pb-1">
+                    <div className="icon">
+                        <FontAwesomeIcon icon={faCalendarDays} />
+                    </div>
+                    <input class="sep form-control form-control-lg input ps-5" type="text" placeholder="Pick-up date"/>
+                </div>
+
+                <div className="wrapper pb-1">
+                    <div className="icon">
+                        <FontAwesomeIcon icon={faClock} />
+                    </div>
+                    <input class="sep form-control form-control-lg input ps-5" type="text" placeholder="Time"/>
+                </div>
+
+                <div className="wrapper pb-1">
+                    <div className="icon">
+                        <FontAwesomeIcon icon={faCalendarDays} />
+                    </div>
+                    <input class="sep form-control form-control-lg input ps-5" type="text" placeholder="Drop-off date"/>
+                </div>
+
+                <div className="wrapper pb-1">
+                    <div className="icon">
+                        <FontAwesomeIcon icon={faClock} />
+                    </div>
+                    <input class="sep form-control form-control-lg input ps-5" type="text" placeholder="Time"/>
+                </div>
+                
+                
+                
+                
             {/* Testing that react has communication with flask */}
             {/* <button className="btn btn-success sep" onClick={loadCatalog}>Search</button> */}
             
             {/* adding link for navigation until search funtionality is implimented */}
-            <Link className="btn btn-success sep" to="/catalog">Search</Link>
+            <Link className="btn btn-success sep p-3" to="/catalog">Search</Link>
+            </div>
+            
         </div>
 
     </div>

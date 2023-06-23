2e9acce51e355d1bf4479162cb1c907f97ca2c8a
Will <wmplqnjr@gmail.com>
2023-06-22 17:53:53 -0700

    deleted

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,20 +2,3 @@ import "./home.css";
 import { Link } from 'react-router-dom'
 import DataService from "../services/dataService";
 
-function Home() {
-
-    function loadCatalog(){
-        let service = new DataService();
-        service.getProducts();
-    }
-
-    return (
-    <div className = "home">
-
-
-
-    </div>
-    );
-}
-
-export default Home

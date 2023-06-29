ca9ad639a57f55e82bc333cdb1685f2af8a1cea7
Will <wmplqnjr@gmail.com>
2023-06-29 14:50:56 -0700

    css

--- src/pages/checkout.jsx
+++ src/pages/checkout.jsx
@@ -1,4 +1,5 @@
-
+import './checkout.css';
+import Product from '../components/product';
 
 
 function Checkout() {
@@ -6,16 +7,19 @@ function Checkout() {
 
     return (
         <div className="checkout">
-            <div className="vehical-image">
-                {/* add image here of selected product */}
-                
+            <div className='product-box'>
+                <div className="vehical-image">
+                    {/* add image here of selected product */}
+                    <p>img</p>
+                </div>
             </div>
 
-            <div className="vehical-description">
-                {/* add selected product description and name here */}
-            </div>
+                <div className="vehical-description">
+                    {/* add selected product description and name here */}
+                    <p>import product</p>
+                </div>
 
-            <div className="order-info">
+                <div className="order-info">
 
 
                 <div className="price-box">
@@ -25,6 +29,8 @@ function Checkout() {
 
                     <div className="price-breakdown">
                         {/* add breakdown of product price including all fees */}
+                        <p>fees</p>
+                        <p>price of car</p>
                     </div>
 
                     <div className="outer-layer">
@@ -36,11 +42,13 @@ function Checkout() {
 
 
                 <div className="email">
+                    <p>Email:</p><input type="text" />
                     {/* add email text box here */}
                 </div>
 
 
                 <div className="button">
+                    <button>Place Order</button>
                     {/* add place reservation button here */}
                 </div>
 

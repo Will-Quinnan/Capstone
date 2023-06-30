a2fe14184b742c626066d6e7ffa6265d4d6d2e83
Brayan Ornelas <ornelas.brayan@uabc.edu.mx>
2023-06-30 14:08:08 -0700

    trip-info updated

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -2,6 +2,8 @@ import { useContext, useEffect, useState } from "react";
 import "./product.css"
 import { Link } from "react-router-dom";
 import StoreContext from "../store/storeContext";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faGaugeHigh } from '@fortawesome/free-solid-svg-icons'
 
 
 function Product(props) {
@@ -29,8 +31,11 @@ function Product(props) {
                 <img src={"/images/"+ props.data.image} className="" alt="car" />
             </div>
 
-            <div>
-                <p>This vehicle  has a {props.data.drive} and {props.data.seats} seats.</p>
+            <div className="ms-5">
+                {/* <p>This vehicle  has a {props.data.drive} and {props.data.seats} seats.</p> */}
+                <p><FontAwesomeIcon icon={faGaugeHigh} /> {props.data.drive}</p>
+                <p><FontAwesomeIcon icon={faUser} /> {props.data.seats} seats</p>
+
                 
                 <div className="price" >
                     <h5 id="product-price">${props.data.price}</h5>
@@ -42,7 +47,7 @@ function Product(props) {
         </div>
 
         <div className="viewDeal">
-            <Link className="btn btn-secondary button-booking" to="/checkout" onClick={Info}>View Deal</Link>
+            <Link className="btn btn-success button-booking" to="/checkout" onClick={Info}>View Deal</Link>
         </div>
     </div>
 
